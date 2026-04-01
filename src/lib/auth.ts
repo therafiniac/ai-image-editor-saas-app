862a684fefff6a04ebebe7c67e9e5214048966a2
Rafi <77590285+therafiniac@users.noreply.github.com>
2026-04-01 05:45:02 +0530

    Feature/implement auth
    * feat(db): create schema and connect to neon database
    
    * feat(auth): add better-auth
    
    * chore: upodate zod version to latest
    
    * feat: initial commit
    
    * chore: add shadcn
    
    * feat(ui): add sooner to layout for toaster
    
    * feat(page): add login page
    
    * refactor(theme): change tailwind class name

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
